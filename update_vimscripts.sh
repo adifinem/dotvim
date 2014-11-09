67dd7b3a91388462da99475f25438d0493bd216e
Jessica Hutchison <j@splatlabs.com>
2014-11-08 23:29:49 -0700

    remove npm/grunt stuff, create install.sh

---
+++ update_vimscripts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl -# -o vim/plugin/tetris.vim http://www.vim.org/scripts/download_script.php?src_id=650
+curl -# -o vim/plugin/matrix.vim http://www.vim.org/scripts/download_script.php?src_id=18650
+curl -# -o vim/plugin/hjkl.vim http://www.vim.org/scripts/download_script.php?src_id=19242
+curl -# -o vim/plugin/gol.vim http://www.vim.org/scripts/download_script.php?src_id=1119
+curl -# -o vim/plugin/mosalisp.vim http://www.vim.org/scripts/download_script.php?src_id=7526
+curl -# -o vim/plugin/nibble.zip http://www.vim.org/scripts/download_script.php?src_id=6711
+curl -# -o vim/plugin/sodoku.vim http://www.vim.org/scripts/download_script.php?src_id=15641
+unzip vim/plugin/nibble.zip -d vim/
+rm vim/plugin/nibble.zip
+
+# git submodule update --init --recursive
+
