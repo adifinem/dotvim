c4a3b43d5ff11b668506ab09d4423812b4c023f1
Jessica Hutchison <j@splatlabs.com>
2014-11-10 11:41:17 -0700

    remove slow lisp addon, add conque term, add default vimrc

--- update_vimscripts.sh
+++ update_vimscripts.sh
@@ -7,8 +7,12 @@ curl -# -o vim/plugin/gol.vim http://www.vim.org/scripts/download_script.php?src
 curl -# -o vim/plugin/mosalisp.vim http://www.vim.org/scripts/download_script.php?src_id=7526
 curl -# -o vim/plugin/nibble.zip http://www.vim.org/scripts/download_script.php?src_id=6711
 curl -# -o vim/plugin/sodoku.vim http://www.vim.org/scripts/download_script.php?src_id=15641
+curl -# -o vim/plugin/conque.zip https://conque.googlecode.com/files/conque_2.3.zip
 unzip vim/plugin/nibble.zip -d vim/
 rm vim/plugin/nibble.zip
+unzip vim/plugin/conque.zip -d vim/
+rm vim/plugin/conque.zip
+
 
 # git submodule update --init --recursive
 
